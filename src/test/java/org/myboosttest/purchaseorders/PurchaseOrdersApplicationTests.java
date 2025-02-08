d4fd4695d0ad0c62b691bf5827f730d335fdb7c3
manz07 <nurmanshaleh@gmail.com>
2025-02-08 09:14:12 +0700

    Initial commit

---
+++ src/test/java/org/myboosttest/purchaseorders/PurchaseOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myboosttest.purchaseorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseOrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
